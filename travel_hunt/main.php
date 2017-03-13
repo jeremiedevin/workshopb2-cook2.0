110d7d9cc3cdfd5274a7eaab3b3d6ca8fadebada
Jérémie D <jeremie.de20@gmail.com>
2017-03-13 15:39:03 +0100

    12eme

--- travel_hunt/main.php
+++ travel_hunt/main.php
@@ -5,7 +5,7 @@ require('secu/plugin-co.php');
 
 <html lang="fr">
 <head>
-<title>K & E - Groupe</title>
+<title>Viz'Eat By K&E</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="keywords" content="Travel Hunt App Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android  Compatible web template, Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
