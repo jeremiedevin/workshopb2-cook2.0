6c9ecd8f842c15a30ede9b3202675d2942e87dd4
Unknown <emmanuel.mars@epsi.fr>
2017-03-14 13:40:18 +0100

    Menu.googlemap

---
+++ front/connexion.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Viz'Eat</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Accueil</a></li>
+      <li><a href="#">Visites</a></li>
+      <li><a href="#">Restaurant</a></li>
+    </ul>
+    <button style="float:right" class="btn btn-danger navbar-btn"><a href="Connexion.php">Connexion/Inscription</a></button>
+  </div>
+
+
+  <div>
+    <form>
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+        <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+      </div>
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+      </div>
+<div class="checkbox">
+<label><input type="checkbox"> Remember me</label>
+</div>
+<button type="submit" class="btn btn-default">Submit</button>
+</form>
+  </div>
