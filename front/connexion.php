3013e4ada769fee3200e82deb26ca251e5f8790a
Jérémie D <jeremie.de20@gmail.com>
2017-03-14 13:53:02 +0100

    connexion

--- front/connexion.php
+++ front/connexion.php
@@ -1,30 +1,39 @@
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="#">Viz'Eat</a>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <?php require('head.php'); ?>
+  </head>
+  <body>
+
+    <?php require('menu.php'); ?>
+
+
+
+    <!-- connexion -->
+
+    <div>
+      <form>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+          <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+        </div>
+        <div class="input-group">
+          <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+          <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+        </div>
+      <div class="checkbox">
+      <label><input type="checkbox"> Remember me</label>
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+  </form>
     </div>
-    <ul class="nav navbar-nav">
-      <li class="active"><a href="#">Accueil</a></li>
-      <li><a href="#">Visites</a></li>
-      <li><a href="#">Restaurant</a></li>
-    </ul>
-    <button style="float:right" class="btn btn-danger navbar-btn"><a href="Connexion.php">Connexion/Inscription</a></button>
-  </div>
+
+    <!-- fin de connexion -->
 
 
-  <div>
-    <form>
-      <div class="input-group">
-        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-        <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-      </div>
-      <div class="input-group">
-        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
-      </div>
-<div class="checkbox">
-<label><input type="checkbox"> Remember me</label>
-</div>
-<button type="submit" class="btn btn-default">Submit</button>
-</form>
-  </div>
+    <?php require('footer.php') ?>
+
+  </body>
+</html>
