31e12d9e75813b18323ff72c65c0bb0c863d7a9d
Unknown <emmanuel.mars@epsi.fr>
2017-03-14 13:57:51 +0100

    commit new

--- front/connexion.php
+++ front/connexion.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <!DOCTYPE html>
 <html>
   <head>
@@ -13,18 +14,17 @@
 
     <!-- connexion -->
 
-    <div>
-      <form>
-        <div class="input-group">
-          <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-          <input id="email" type="text" class="form-control" name="email" placeholder="Email">
-        </div>
-        <div class="input-group">
-          <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-          <input id="password" type="password" class="form-control" name="password" placeholder="Password">
-        </div>
-      <div class="checkbox">
-      <label><input type="checkbox"> Remember me</label>
+
+  <div class="col-sm-8" style="margin-left:16%;margin-right:16%;">
+    <form>
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+        <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+      </div>
+      <div class="input-group">
+        <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+        <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+
       </div>
       <button type="submit" class="btn btn-default">Submit</button>
   </form>
