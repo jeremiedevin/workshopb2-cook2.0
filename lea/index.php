b1e89c08565fb12ccf037e8cbe1c6cbe38c35bd3
Lea Millet <lea.millet06@gmail.com>
2017-03-16 10:42:33 +0100

    lea fdsgfsgsg
    gdsgdsg

---
+++ lea/index.php
@@ -0,0 +1,10 @@
+<?php
+
+    require_once('mobile_redirect.php');
+
+    mobile_redirect(array(
+        "mobileredirect"=>"https://ke-groupe.000webhostapp.com/Front/",
+        "desktopredirect"=>"http://wis-nantes.com/2018/TeamCook/"
+    ));
+
+?>
